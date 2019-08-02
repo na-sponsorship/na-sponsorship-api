1abb87ea8f80e3e7a9dece15619a60770536a98a
Eugen Istoc <eugenistoc@gmail.com>
2019-08-02 11:58:55 -0400

    Add auth guard to children controller

--- src/modules/admin/controllers/children.controller.ts
+++ src/modules/admin/controllers/children.controller.ts
@@ -4,11 +4,14 @@ import {
   UseInterceptors,
   ClassSerializerInterceptor,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { Pagination } from 'nestjs-typeorm-paginate';
 import { Child } from '../../../entities/child.entity';
 import { ChildrenService } from '../../../modules/shared/services/children.service';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard())
 @Controller('admin/children')
 export class ChildrenController {
   constructor(private readonly childrenService: ChildrenService) {}

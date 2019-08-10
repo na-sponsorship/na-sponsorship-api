6b53ebcc97a9b26daef3669cf635a6b61ea5bec6
Eugen Istoc <eugenistoc@gmail.com>
2019-08-10 16:19:35 -0400

    Stub out S3Storage provider for uploading to s3 in Production and development environments

---
+++ src/modules/admin/storage-providers/s3.storage.ts
@@ -0,0 +1,8 @@
+export const S3Storage = {
+  _handleFile(req, file, cb) {
+    console.log('handle s3 upload');
+  },
+  _removeFile(req, file, cb) {
+    console.log('remove file from s3');
+  },
+};

e364cb51b2a49d7cc087cd63d29de3372666e74f
Eugen Istoc <eugenistoc@gmail.com>
2019-08-10 16:18:49 -0400

    Add DiskStorage provider for local file uploads

---
+++ src/modules/admin/storage-providers/disk.storage.ts
@@ -0,0 +1,12 @@
+import * as multer from 'multer';
+import * as path from 'path';
+import { uniqueId } from 'lodash';
+
+export const DiskStorage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, path.join(__dirname, '../../../../uploads'));
+  },
+  filename(req, file, cb) {
+    cb(null, `child-${Date.now()}.${file.originalname.split('.').pop()}`);
+  },
+});

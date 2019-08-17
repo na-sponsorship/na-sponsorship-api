de34bbbed39e45cc64c75da6cb8d90afb36559ce
Eugen Istoc <eugenistoc@gmail.com>
2019-08-17 15:23:59 -0400

    Remove config in favor of env variables

--- src/config/sentry.ts
+++
@@ -1,3 +0,0 @@
-export default {
-    URL: process.env.SENTRY_URL,
-};
--- src/config/stripe.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    APIKEY: process.env.STRIPE_APIKEY,
-    WBHOOK_SIGNATURE: process.env.STRIPE_WBHOOK_SIGNATURE,
-};

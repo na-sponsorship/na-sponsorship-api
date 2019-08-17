8766059bafe80cad5fbe65b93080018983a74790
Eugen Istoc <eugenistoc@gmail.com>
2019-08-17 15:23:45 -0400

    add class validations

--- src/modules/admin/dto/children/createChild.dto.ts
+++ src/modules/admin/dto/children/createChild.dto.ts
@@ -1,9 +1,20 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsEnum,
+  IsNumberString,
+  IsDateString,
+} from 'class-validator';
 
+enum Gender {
+  Male = 'male',
+  Female = 'female',
+}
 export class CreateChildDTO {
-  @IsNotEmpty()
-  readonly firstName: string;
-
-  @IsNotEmpty()
-  readonly lastName: string;
+  @IsNotEmpty() readonly firstName: any;
+  @IsNotEmpty() readonly lastName: any;
+  @IsDateString() readonly dateOfBirth: any;
+  @IsNumberString() readonly grade: any;
+  @IsNotEmpty() readonly story: any;
+  @IsEnum(Gender) readonly gender: any;
+  @IsNumberString() readonly sponsorsNeeded: any;
 }

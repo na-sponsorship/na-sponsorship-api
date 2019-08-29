be4c44dab11867aaa293a70d0802e9a5e465fcfc
Eugen Istoc <eugenistoc@gmail.com>
2019-08-29 11:15:16 -0400

    Fix issue with dotenv config being loaded too late

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SentryLogger } from './sentry.logger';
-import * as dotenv from 'dotenv';
 import * as path from 'path';
 import { ValidationPipe } from '@nestjs/common';
 
-dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {

f9bfcdbd1afb0958c4c1d3226c872dc292a5a25b
Eugen Istoc <eugenistoc@gmail.com>
2019-09-10 12:05:41 -0400

    set asset path correctly when running on local vs production

--- src/main.ts
+++ src/main.ts
@@ -10,16 +10,20 @@ import * as path from 'path';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
+const assetPath =
+  process.env.NODE_ENV === 'local'
+    ? join(__dirname, '..', 'public')
+    : join(__dirname, '../../', 'public');
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: new SentryLogger(process.env.SENTRY_URL),
     bodyParser: true,
     cors: true,
   });
-  const p = join(__dirname, '..', 'public');
-  console.log(`public folder: ${p}`)
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.useStaticAssets(p);
+  app.useStaticAssets(assetPath);
   await app.listen(process.env.PORT);
 }
 bootstrap();

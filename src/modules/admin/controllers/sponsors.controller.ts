0dad12c4b04186274c1dead32bcdf37c30dfa8b9
Eugen Istoc <eugenistoc@gmail.com>
2019-10-21 15:08:08 -0400

    cleanup and refactoring

--- src/modules/admin/controllers/sponsors.controller.ts
+++ src/modules/admin/controllers/sponsors.controller.ts
@@ -1,18 +1,9 @@
-import { Controller, Get, UseInterceptors, UseGuards, Post, UploadedFile, Body, Delete, Param, Put, Header, Res } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { get, each } from 'lodash';
-import * as Stripe from 'stripe';
 
-import { Child } from '../../../entities/child.entity';
-import { ChildrenService } from '../../shared/services/children.service';
-import { CreateChildDTO } from '../dto/children/createChild.dto';
-import { StripeService } from '../../shared/services/vendors/stripe.service';
-import { CloudinaryService } from '../../shared/services/vendors/cloudinary.service';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { UpdateChildDTO } from '../dto/children/updateChild.dto';
-import { Sponsor } from 'src/entities/sponsor.entity';
+import { Sponsor } from '../../../entities/sponsor.entity';
 
 @UseGuards(AuthGuard())
 @Controller('admin/sponsors')
